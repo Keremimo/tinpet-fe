0e725645f7e2de447be56abdb54fbd9959d5be11
Kerem Kilic <code@kerem.tech>
2024-12-13 16:44:13 +0100

    Attempt to fix logout not detecting login status (Failed)

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,66 +1,75 @@
-'use client';
+"use client"
 
-import { useState } from "react";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { useState } from "react"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+import { useRouter } from "next/navigation"
 
 const LoginPage = () => {
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [successMessage, setSuccessMessage] = useState(null);
-  const [password, setPassword] = useState('');
-  const router = useRouter();
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [successMessage, setSuccessMessage] = useState(null)
+  const [password, setPassword] = useState("")
+  const router = useRouter()
 
-  const onSubmit = async (data) => { 
-    const { username, password } = data;
+  const onSubmit = async (data) => {
+    const { username, password } = data
 
     try {
-      const response = await fetch('http://localhost:3001/api/v1/login', {
-        method: 'POST',
-        "Access-Control-Allow-Origin": 'http://localhost:3001',
-        headers: { 'Content-Type': 'application/json' },
+      const response = await fetch("http://localhost:3001/api/v1/login", {
+        method: "POST",
+        "Access-Control-Allow-Origin": "http://localhost:3001",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
-        credentials: 'include',
-      });
+        credentials: "include",
+      })
 
-      const result = await response.json();
+      const result = await response.json()
 
       if (response.ok) {
-        setSuccessMessage(result.message || "You logged in!");
-        localStorage.setItem("login", result);
-        setTimeout(() => router.push('/'), 1000);
-        
+        setSuccessMessage(result.message || "You logged in!")
+        console.log(result)
+        localStorage.setItem("login", JSON.stringify(result.user))
+        setTimeout(() => router.push("/"), 1000)
+
         setErrorMessage(null)
       } else {
-        setErrorMessage(result.message || "Something went wrong when trying to log in.");
+        setErrorMessage(
+          result.message || "Something went wrong when trying to log in."
+        )
       }
     } catch (err) {
-      setErrorMessage("An error occurred while trying to log in.");
-      console.log(err.message);
+      setErrorMessage("An error occurred while trying to log in.")
+      console.log(err.message)
     }
-  };
+  }
 
-
-    return (
-        <div className="max-w-md mx-auto p-6 bg-white rounded-lg shadow-md">
-      <h2 className="text-2xl font-semibold mb-4 text-center text-customBrown font-lilita">Welcome back</h2>
+  return (
+    <div className="max-w-md mx-auto p-6 bg-white rounded-lg shadow-md">
+      <h2 className="text-2xl font-semibold mb-4 text-center text-customBrown font-lilita">
+        Welcome back
+      </h2>
 
       {successMessage && (
-        <div className="p-4 mb-4 bg-green-100 text-green-800 rounded">{successMessage}</div>
-        )}
+        <div className="p-4 mb-4 bg-green-100 text-green-800 rounded">
+          {successMessage}
+        </div>
+      )}
       {errorMessage && (
-        <div className="p-4 mb-4 bg-red-100 text-red-800 rounded">{errorMessage}</div>
-        )}
+        <div className="p-4 mb-4 bg-red-100 text-red-800 rounded">
+          {errorMessage}
+        </div>
+      )}
 
-      <form onSubmit={(e) => {
-        e.preventDefault();
-        const formData = new FormData(e.target);
-        const formObj = Object.fromEntries(formData.entries());
-        onSubmit(formObj);
-      }}>
-
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          const formData = new FormData(e.target)
+          const formObj = Object.fromEntries(formData.entries())
+          onSubmit(formObj)
+        }}
+      >
         <div className="mb-4">
           <Label htmlFor="username">Username</Label>
           <Input
@@ -84,13 +93,24 @@ const LoginPage = () => {
           />
         </div>
 
-        <Button type="submit" className="text-center bg-customBrown hover:bg-customDarkBrown">
+        <Button
+          type="submit"
+          className="text-center bg-customBrown hover:bg-customDarkBrown"
+        >
           Login
         </Button>
-        <p className="text-xs text-center">Do not have an acount? Sign up <Link href="/register" className="font-semibold underline text-blue-500">here</Link></p>
+        <p className="text-xs text-center">
+          Do not have an acount? Sign up{" "}
+          <Link
+            href="/register"
+            className="font-semibold underline text-blue-500"
+          >
+            here
+          </Link>
+        </p>
       </form>
     </div>
-    )
+  )
 }
 
-export default LoginPage;
+export default LoginPage
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,82 +1,105 @@
-"use client";
+"use client"
 
-import Link from "next/link";
-import { useState, useEffect } from "react";
-import { Menu } from "lucide-react";
+import Link from "next/link"
+import { useState, useEffect } from "react"
+import { Menu } from "lucide-react"
+import { useRouter } from "next/navigation"
 
-const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false); // State for toggling the mobile menu
-  
-  const [sessionExist, setSessionExist] = useState(() => {
-    if (localStorage.getItem("login")) {
-      return true;
-    } else {
-      return false;
-    }
-  });
+const Navbar = ({ cookieStore }) => {
+  const router = useRouter()
+  const [isOpen, setIsOpen] = useState(false) // State for toggling the mobile menu
+
+  const [sessionExist, setSessionExist] = useState(cookieStore ? true : false)
 
   useEffect(() => {
-    if (localStorage.getItem("login")) {
-      console.log("login or registration exist");
-      setSessionExist(true);
+    if (cookieStore) {
+      console.log("login or registration exist")
+      setSessionExist(true)
     } else {
-      console.log("No registration or login exist");
-      setSessionExist(false);
+      console.log("No registration or login exist")
+      setSessionExist(false)
     }
-  }, []);
+  }, [sessionExist])
 
   async function logOut() {
-    localStorage.removeItem('login');
-    localStorage.removeItem('registration');
     try {
-      const response = await fetch('http://localhost:3001/api/v1/logout', {
-        method: "GET",
-        credentials: 'include'
-      });
+      const response = await fetch("http://localhost:3001/api/v1/logout", {
+        method: "POST",
+        credentials: "include",
+      })
+      if (!response.ok) {
+        throw new Error("Error logging out")
+      }
+      localStorage.removeItem("login")
+      localStorage.removeItem("registration")
+      setSessionExist(false)
+      router.push("/")
     } catch (e) {
-      console.error(e);
+      console.log(e.toString())
     }
   }
-
 
   return (
     <nav className="bg-customBrown  w-auto m-4 rounded">
       <div className="flex justify-between items-center px-4 py-2">
-        <div className="text-white text-lg font-bold">TINPET</div>
+        <Link href={"/"} className="text-white text-lg font-bold">
+          TINPET
+        </Link>
         <div className="hidden md:flex space-x-4">
           {" "}
           {/* Only show on medium screens and up */}
           <Link href="/" className="text-white  text-lg hover:underline">
             Home
           </Link>
-          <Link href="/our-pets" className="text-white text-lg  hover:underline">
+          <Link
+            href="/our-pets"
+            className="text-white text-lg  hover:underline"
+          >
             Our Pets
           </Link>
-          <Link href="/pet-form" className="text-white text-lg  hover:underline">
+          <Link
+            href="/pet-form"
+            className="text-white text-lg  hover:underline"
+          >
             Put up for adoption
           </Link>
           <Link href="/about" className="text-white text-lg  hover:underline">
             About
           </Link>
-          <Link href="/contact-us" className="text-white text-lg hover:underline">
+          <Link
+            href="/contact-us"
+            className="text-white text-lg hover:underline"
+          >
             Contact Us
           </Link>
-          {!sessionExist && <Link href="/register" className="text-white text-lg hover:underline">
-            Register
-          </Link>}
-          {!sessionExist && <Link href="/login" className="text-white text-lg hover:underline">
-            Log in
-          </Link>}
-          {sessionExist && <button onClick={() => {logOut()}}
-            href="/"
-            className="block text-white text-lg hover:underline">
-            Log Out
-          </button>}
-          
+          {!sessionExist && (
+            <Link
+              href="/register"
+              className="text-white text-lg hover:underline"
+            >
+              Register
+            </Link>
+          )}
+          {!sessionExist && (
+            <Link href="/login" className="text-white text-lg hover:underline">
+              Log in
+            </Link>
+          )}
+          {sessionExist && (
+            <button
+              onClick={() => {
+                logOut()
+              }}
+              className="block text-white text-lg hover:underline"
+            >
+              Log Out
+            </button>
+          )}
         </div>
         <button
           onClick={() => setIsOpen(!isOpen)}
-          className="md:hidden text-white">
+          className="md:hidden text-white"
+        >
           <Menu className="h-5 w-5" />
         </button>
       </div>
@@ -88,43 +111,57 @@ const Navbar = () => {
           </Link>
           <Link
             href="/pets"
-            className="block text-white text-lg hover:underline">
+            className="block text-white text-lg hover:underline"
+          >
             Our Pets
           </Link>
           <Link
             href="/pet-form"
-            className="block text-white text-lg hover:underline">
+            className="block text-white text-lg hover:underline"
+          >
             Put up for adoption
           </Link>
           <Link
             href="/about"
-            className="block text-white text-lg hover:underline">
+            className="block text-white text-lg hover:underline"
+          >
             About
           </Link>
           <Link
             href="/contact"
-            className="block text-white text-lg hover:underline">
+            className="block text-white text-lg hover:underline"
+          >
             Contact Us
           </Link>
-          {!sessionExist && <Link
-            href="/register"
-            className="block text-white text-lg hover:underline">
-            Register
-          </Link>}
-          {!sessionExist &&<Link
-            href="/login"
-            className="block text-white text-lg hover:underline">
-            Log In
-          </Link>}
-          {sessionExist && <button onClick={() => {logOut()}}
-            href="/"
-            className="block text-white text-lg hover:underline">
-            Log Out
-          </button>}
+          {!sessionExist && (
+            <Link
+              href="/register"
+              className="block text-white text-lg hover:underline"
+            >
+              Register
+            </Link>
+          )}
+          {!sessionExist && (
+            <Link
+              href="/login"
+              className="block text-white text-lg hover:underline"
+            >
+              Log In
+            </Link>
+          )}
+          {sessionExist && (
+            <button
+              onClick={logOut}
+              href="/"
+              className="block text-white text-lg hover:underline"
+            >
+              Log Out
+            </button>
+          )}
         </div>
       )}
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
